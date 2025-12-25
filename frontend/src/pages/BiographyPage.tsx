45c69bb86f7b3f9b80270012eb140ead1364bd51
Marija Stakic <marijastakic67@gmail.com>
2025-12-25 20:41:19 +0100

    Biography page v1

--- frontend/src/pages/BiographyPage.tsx
+++ frontend/src/pages/BiographyPage.tsx
@@ -1,9 +1,41 @@
 import BottomNav from "../components/BottomNav";
+import './BiographyPage.css'
 
 export default function BiographyPage() {
   return(
     <>
-    <BottomNav />
+      <div className="biography-container">
+        <div className="biography-top">
+            <img src="/images/nikiBio.jpeg" className="bio-image" />
+            <div className="nikolija-bio">Nikolija Stanojević</div>
+            <div className="page-title suisse-large">biography</div>
+        </div>
+
+        <div className="about-nikolija suisse-text">
+              Lorem ipsum dolor sit amet consectetur. Augue pretium est nisl id duis velit sit hendrerit. Nibh eget mattis feugiat mauris donec suspendisse. Cursus risus nisl dui ornare dignissim aliquet massa. Ultrices mauris a purus a pretium ut fringilla. Sem velit in feugiat in. Et viverra faucibus quis ac consectetur molestie. 
+              Felis sodales in maecenas malesuada in vitae mauris eget risus. 
+              Rhoncus feugiat sem velit nisl urna ut in neque. 
+              Morbi rhoncus enim aenean odio sed.
+              At habitasse convallis porta nisl nibh nibh diam. 
+              Orci tincidunt malesuada accumsan pellentesque.
+              Aliquam tempus aliquam viverra adipiscing fermentum sed. 
+              <br />
+              Id faucibus at urna lectus felis nisl iaculis.
+                Lectus consectetur neque malesuada neque magna in.
+                Et dignissim vitae enim elementum ultricies magna vivamus lobortis lorem.
+                  Nulla urna mi dictum magnis massa.
+              Tenim placerat pharetra sit. 
+              Nulla nunc sit gravida interdum facilisis facilisis gravida sit id. 
+              Malesuada tristique eget faucibus consectetur odio consectetur ac. <br />
+              Ut eu habitant odio amet commodo orci morbi odio malesuada. 
+              Fermentum non suspendisse proin non mattis diam suspendisse id vitae.
+              Massa adipiscing ultrices amet volutpat integer bibendum in tellus. 
+              Faucibus dolor amet suspendisse nunc sit senectus.
+        </div>
+
+      </div>
+
+      <BottomNav />
     </>
   )
 }
